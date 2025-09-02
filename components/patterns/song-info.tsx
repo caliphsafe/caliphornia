6e8ce240d96f1353a7d5561b221468d199bc37b0
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2025-09-02 04:16:48 -0400

    Update song-info.tsx

--- components/patterns/song-info.tsx
+++ components/patterns/song-info.tsx
@@ -5,7 +5,7 @@ interface SongInfoProps {
 export function SongInfo({ className }: SongInfoProps) {
   return (
     <div className={`text-center ${className}`}>
-      <h2 className="text-black text-[32px] md:text-[48px] font-black tracking-tight ">OPTIONS</h2>
+      <h2 className="text-black text-[32px] md:text-[48px] font-black tracking-tight ">POLYGAMY (PROD BY. CALIPH)</h2>
       <p className="text-[#A0896B] text-xl md:text-2xl font-medium tracking-wide mb-12">CALIPH</p>
     </div>
   )
