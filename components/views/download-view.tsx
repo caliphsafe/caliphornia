e71b84fcf904c2dde8874d2b5b9a05e42d13fa61
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2025-09-02 04:47:06 -0400

    Update download-view.tsx

--- components/views/download-view.tsx
+++ components/views/download-view.tsx
@@ -30,7 +30,7 @@ export function DownloadView() {
 
       {/* Options Title */}
       <div className="text-center mb-8">
-        <h1 className="font-bold text-black text-[32px] md:text-[48px]">OPTIONS</h1>
+        <h1 className="font-bold text-black text-[32px] md:text-[48px]">POLYGAMY</h1>
       </div>
 
       {/* Album Cover */}
