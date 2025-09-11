c19b54e7bbad46bd595ed5889dc6439073e943f8
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2025-09-11 15:29:18 -0400

    Create footer.tsx

---
+++ components/patterns/footer.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export function Footer() {
+  return (
+    <footer className="w-full text-center py-6 text-xs md:text-sm font-medium" style={{ color: "#4a3f35" }}>
+      POWERED BY KIIKU © 2025
+    </footer>
+  )
+}
